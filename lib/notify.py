fc639ec1945b800ff6135e4f38e0e6ed00efb24b
Zachary Miller <codebyzach@gmail.com>
2021-03-24 18:24:36 -0400

    Back to spaces

--- lib/notify.py
+++ lib/notify.py
@@ -1,33 +1,33 @@
 """Add SubNotify support messages if SubNotify is installed."""
 import sublime
 try:
-	from SubNotify.sub_notify import SubNotifyIsReadyCommand as Notify
+    from SubNotify.sub_notify import SubNotifyIsReadyCommand as Notify
 except Exception:
-	class Notify(object):
-		"""Fallback Notify for when SubNotify is not defined."""
+    class Notify(object):
+        """Fallback Notify for when SubNotify is not defined."""
 
-		@classmethod
-		def is_ready(cls):
-			"""Return false to disable SubNotify usage."""
+        @classmethod
+        def is_ready(cls):
+            """Return false to disable SubNotify usage."""
 
-			return False
+            return False
 
 
 def notify(msg):
-	"""Notify message."""
+    """Notify message."""
 
-	settings = sublime.load_settings("ExportHtml.sublime-settings")
-	if settings.get("use_sub_notify", False) and Notify.is_ready():
-		sublime.run_command("sub_notify", {"title": "ExportHtml", "msg": msg})
-	else:
-		sublime.status_message(msg)
+    settings = sublime.load_settings("ExportHtml.sublime-settings")
+    if settings.get("use_sub_notify", False) and Notify.is_ready():
+        sublime.run_command("sub_notify", {"title": "ExportHtml", "msg": msg})
+    else:
+        sublime.status_message(msg)
 
 
 def error(msg):
-	"""Error message."""
+    """Error message."""
 
-	settings = sublime.load_settings("ExportHtml.sublime-settings")
-	if settings.get("use_sub_notify", False) and Notify.is_ready():
-		sublime.run_command("sub_notify", {"title": "ExportHtml", "msg": msg, "level": "error"})
-	else:
-		sublime.error_message("ExportHtml:\n%s" % msg)
+    settings = sublime.load_settings("ExportHtml.sublime-settings")
+    if settings.get("use_sub_notify", False) and Notify.is_ready():
+        sublime.run_command("sub_notify", {"title": "ExportHtml", "msg": msg, "level": "error"})
+    else:
+        sublime.error_message("ExportHtml:\n%s" % msg)
